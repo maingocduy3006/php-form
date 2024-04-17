71b03f9115e1ccbe69406e5cbefa4084548676fa
maingocduy3006 <108670007+maingocduy3006@users.noreply.github.com>
2024-04-17 22:50:51 +0200

    form done

---
+++ message.php
@@ -0,0 +1,30 @@
+//Contact Form in PHP
+<?php
+ini_set('SMTP', 'smtp.gmail.com');
+ini_set('smtp_port', '587');
+ini_set('SMTPSecure', 'tls');
+
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $phone = htmlspecialchars($_POST['phone']);
+  $website = htmlspecialchars($_POST['website']);
+  $message = htmlspecialchars($_POST['message']);
+
+  if(!empty($email) && !empty($message)){
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+      $receiver = "duy2002.1@gmail.com"; //enter that email address where you want to receive all messages
+      $subject = "From: $name <$email>";
+      $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
+      $sender = "From: $email";
+      if(mail($receiver, $subject, $body, $sender)){
+         echo "Your message has been sent";
+      }else{
+         echo "Sorry, failed to send your message!";
+      }
+    }else{
+      echo "Enter a valid email address!";
+    }
+  }else{
+    echo "Email and message field is required!";
+  }
+?>
